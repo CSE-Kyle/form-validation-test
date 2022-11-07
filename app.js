55e936187c921a4aaf79fff2dec85fef52d290b4
Kyle Michael Deguzman <kylemichaeldeguzman@Kyles-MBP.attlocal.net>
2022-11-06 19:48:34 -0800

    added form validation for all inputs, checking whether or not if there's any input in each of the input sections

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 function formSubmit() {
-    const regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
-    const name = document.querySelector("#name-input").value;
+   const regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+   const name = document.querySelector("#name-input").value;
 
-    if(!regName.test(name)) {
-       alert("Please fill out required information");
-    } else {
-       alert("Form submitted, thank you!");
-       formSubmit.submit();
-       formSubmit.reset();
-       return false;
-    }
+   const regEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+   const email = document.querySelector("#email-input").value;
+
+   const radio = document.querySelector('input[name="radio"]:checked');
+   const radioValue = radio ? radio.value : null;
+
+   const textarea = document.querySelector("#text-area").value;
+   const textareaValue = textarea ? textarea : null;
+
+   if(!regName.test(name) || !regEmail.test(email) || !radioValue || !textareaValue) {
+      alert("Please fill out required information");
+   } else {
+      alert("Form submitted, thank you!");
+      formSubmit.submit();
+      formSubmit.reset();
+      return false;
+   }
 }
